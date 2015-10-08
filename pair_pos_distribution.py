90aed9c09ccf7c2751e3d86e897b596fd164e97a
ShenglanQiao <shenglan@stanford.edu>
2015-10-07 17:16:10 -0700

    calculate pairwise distances between waters

--- pair_pos_distribution.py
+++ pair_pos_distribution.py
@@ -12,6 +12,10 @@
 
 import mdtraj as md
 
+import numpy as np
+from itertools import combinations
+import matplotlib.pyplot as plt
+
 ##############################################################################
 # Code
 ##############################################################################
@@ -21,7 +25,12 @@ traj = md.load_trr(data_path+'/nvt-pr.trr', top = data_path+'/water-sol.gro')
 print ('here is some info about the trajectory we are looking at:')
 print traj
 
-#atom.index for all Oxygen of the water molecules
+#atom.index for all Oxygen of the water molecules, get pariwise distances
 water_inds = traj.topology.select_atom_indices(selection='water')
+water_pairs = np.array(list(combinations(sorted(water_inds),2)))
+water_dist = md.compute_distances(traj,water_pairs)
 
+print water_dist[0].shape
 
+plt.hist(water_dist[0])
+plt.show()
