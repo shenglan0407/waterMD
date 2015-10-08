42e99d7e2095a71590f00c6c9567bf7bfffdee7e
ShenglanQiao <shenglan@stanford.edu>
2015-10-08 11:03:39 -0700

    saving figures instead of displaying

--- pair_pos_distribution.py
+++ pair_pos_distribution.py
@@ -20,7 +20,7 @@ import matplotlib.pyplot as plt
 # Code
 ##############################################################################
 
-data_path = '/Users/shenglanqiao/Documents/GitHub/waterMD/data'
+data_path = '/Users/shenglanqiao/zauber/MD_simulations/water_box/cubic_1.2nm'
 traj = md.load_trr(data_path+'/nvt-pr.trr', top = data_path+'/water-sol.gro')
 print ('here is some info about the trajectory we are looking at:')
 print traj
@@ -38,17 +38,23 @@ sd_dist = np.std(water_dist,axis=1)
 traj_time = np.array(range(len(mean_dist)))*time_step
 
 #visualize
-f, axarr = plt.subplots(2, sharex=True)
+fig1, axarr = plt.subplots(2, sharex=True)
 axarr[0].scatter(traj_time, mean_dist)
+axarr[0].set_ylim([1.2325,1.2335])
 axarr[0].set_ylabel('mean pairwise distances (nm)')
 axarr[1].scatter(traj_time, sd_dist)
+axarr[1].set_ylim([0.353,0.354])
 axarr[1].set_ylabel('std pairwise distances (nm)')
 axarr[1].set_xlabel('time(ps)')
 
-plt.show()
+fig1.savefig('/Users/shenglanqiao/Documents/Github/waterMD/output/pairwise_timeseries.png')
+plt.close(fig1)
 
+
+fig2 = plt.figure()
 for this_frame in water_dist:
     plt.hist(this_frame,alpha=0.05)
 plt.title('Distribution of pairwise water distances superimposed by frame')
 plt.xlabel('Pairwise distances (nm)')
-plt.show()
+fig2.savefig('/Users/shenglanqiao/Documents/Github/waterMD/output/pairwise_histogram.png')
+plt.close(fig2)
