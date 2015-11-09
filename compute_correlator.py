c81164a401d1c2a47389328f85d177d7b0867063
shenglan <shenglan@stanford.edu>
2015-11-09 15:11:56 -0800

    script for computing correlators

---
+++ compute_correlator.py
@@ -0,0 +1,55 @@
+##############################################################################
+# Copyright 2015 Stanford University and the Author
+#
+# Author: Shenglan Qiao
+#
+# Testing class WaterStats
+#
+# The computation done here is based on the following papers:
+# 
+# [1] Salacuse et al. Finite-size effects of molecular dynamics simulations: static structure 
+# factor and compressibility. I. theoretical method
+#
+# [2] Salacuse et al. Finite-size effects of molecular dynamics simulations: static structure 
+# factor and compressibility. II. application to a model krypton fluid
+#
+#
+#############################################################################
+
+
+##############################################################################
+# Imports
+##############################################################################
+
+from water_stats import WaterStats
+
+import mdtraj as md
+import numpy as np
+
+import os
+import time
+
+##############################################################################
+# Code
+##############################################################################
+
+data_path = os.getcwd()+'/data'
+run_name = 'run2'
+traj = md.load_trr(data_path+'/nvt-pr_'+run_name+'.trr', top = data_path+'/water-sol_'+run_name+'.gro')
+print ('here is some info about the trajectory we are looking at:')
+print traj
+run2 = WaterStats(traj,'run2')
+
+q = 1/0.3*np.pi*2.0
+theta_1 = np.pi/12.
+phi = np.linspace(-np.pi,np.pi,10)
+dt = 1.0 # ps
+frames = np.arange(101)[75:]
+
+tic = time.clock()
+run2.correlator(q,theta_1,frames,phi,cut_off = 0.5)
+toc = time.clock()
+
+print("Correlator process time: %.2f" %(toc-tic))
+
+run2.all_tthds.close()
