26256b3c4b9a76b28147738ed56100dd898b2587
shenglan <shenglan@stanford.edu>
2015-11-18 11:35:15 -0800

    tried plotting results from run5

--- plot_Corr_generic.py
+++ plot_Corr_generic.py
@@ -24,7 +24,7 @@ import mdtraj as md
 # Code
 ##############################################################################
 
-run_name = 'run1'
+run_name = 'run5'
 data_path = os.getcwd()+'/data'
 traj = md.load_trr(data_path+'/nvt-pr_'+run_name+'.trr', top = data_path+'/water-sol_'+run_name+'.gro')
 print ('here is some info about the trajectory we are looking at:')
@@ -33,9 +33,9 @@ test = WaterStats(traj,run_name,read_mod='r')
 test.all_tthds.close()
 test.nearest_tthds.close()
 
-run_name = 'all'
+# run_name = 'all'
 all_data = []
-with open(os.getcwd()+'/computed_results/combined_corr_all.csv','r') as f:
+with open(os.getcwd()+'/computed_results/combined_corr_run5.csv','r') as f:
         csvreader = csv.reader(f,delimiter=' ',quotechar='|')
         q1=(next(csvreader))
         q2=(next(csvreader))
@@ -53,11 +53,11 @@ all_data = np.array(all_data,dtype=float)
 Corr = [np.mean(all_data[:,ii]) for ii in range(len(psi))]
 C_err = [np.std(all_data[:,ii])/np.sqrt(len(all_data[:,ii])) for ii in range(len(psi))]
 
-
-fig = plt.figure()
-plt.errorbar(psi,Corr,yerr = C_err,fmt='-.')
-plt.title('4-point correlator')
-plt.xlabel('psi')
-plt.ylabel('C')
-fig.savefig('Corr_fine_'+run_name+'_all.png')
-plt.close(fig)
+print 'The average error is %.2f. ' % np.mean(C_err)
+# fig = plt.figure()
+# plt.errorbar(psi,Corr,yerr = C_err,fmt='-.')
+# plt.title('4-point correlator')
+# plt.xlabel('psi')
+# plt.ylabel('C')
+# fig.savefig(os.getcwd()+'/output/Corr_fine_'+run_name+'_everyother.png')
+# plt.close(fig)
