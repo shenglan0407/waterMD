09f83782ae41e8e123154df9bde2a265679b4d24
shenglan <shenglan@stanford.edu>
2015-11-18 15:03:23 -0800

    user input options

--- compute_correlator.py
+++ compute_correlator.py
@@ -28,31 +28,65 @@ import numpy as np
 
 import os
 import time
-
+import sys
+import getopt
 ##############################################################################
 # Code
 ##############################################################################
 
-frames = np.arange(1001)[500:]
-run_name = 'run5'
+def main(argv):
+    run_name = None
+    outputfile = None
+    number_qs = 10
+    try:
+        opts, args = getopt.getopt(argv,"hi:o:p:",["ifile=","ofile=","n_phi="])
+    except getopt.GetoptError:
+        print 'compute_correlator.py -i <runname> -o <outputfile> -p <nphi>'
+        sys.exit(2)
 
-data_path = os.getcwd()+'/data'
-traj = md.load_trr(data_path+'/nvt-pr_'+run_name+'.trr', top = data_path+'/water-sol_'+run_name+'.gro')
-print ('here is some info about the trajectory we are looking at:')
-print traj
-run = WaterStats(traj,run_name,read_mod='r')
+    for opt, arg in opts:
+        if opt == '-h':
+            
+            print 'compute_correlator.py -i <runname> -o <outputfile> -p <nphi>'
+            sys.exit()
+        elif opt in ("-i", "--ifile"):
+            run_name = arg
+        elif opt in ("-o", "--ofile"):
+            outputfile = arg
+        elif opt in ("-p","--n_phi"):
+            number_qs = arg
+    print 'Input run is %s' % run_name
+    print 'Output file is %s'% outputfile
+    print 'Number of phi used is %s'%number_qs
+    
+    if run_name == None:
+        print '<runname> must be provided.'
+        print 'compute_correlator.py -i <runname> -o <outputfile> -p <nphi>'
+        sys.exit(2)
+        
+    data_path = os.getcwd()+'/data'
+    traj = md.load_trr(data_path+'/nvt-pr_'+run_name+'.trr', top = data_path+'/water-sol_'+run_name+'.gro')
+    print ('here is some info about the trajectory we are looking at:')
+    print traj
+    run = WaterStats(traj,run_name,read_mod='r')
+    frames = np.arange(1001)[500:]
 
-q = 1/0.3*np.pi*2.0
-theta_1 = np.pi/12.
-phi = np.linspace(-np.pi/2.,np.pi/2.,25)
-dt = 1.0 # ps
+    q = 1/0.3*np.pi*2.0
+    theta_1 = np.pi/12.
+    phi = np.linspace(-np.pi/2.,np.pi/2.,number_qs)
+    dt = 1.0 # ps
 
 
-tic = time.clock()
-run.correlator(q,theta_1,frames,phi,cut_off = 0.5)
-toc = time.clock()
+    tic = time.clock()
+    run.correlator(q,theta_1,frames,phi,cut_off = 0.5,output=outputfile)
+    toc = time.clock()
 
-print("Correlator process time: %.2f" %(toc-tic))
+    print("Correlator process time: %.2f" %(toc-tic))
 
-run.all_tthds.close()
-run.nearest_tthds.close()
+    run.all_tthds.close()
+    run.nearest_tthds.close()
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
+
+
