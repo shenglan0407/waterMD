fd9eddacf81fd2583fa3e8d7fb4ce6f02a559b99
shenglan <shenglan@stanford.edu>
2015-12-15 15:11:39 -0800

    timing processes

--- assign_nearest_tthds.py
+++ assign_nearest_tthds.py
@@ -32,9 +32,9 @@ import time
 # Code
 ##############################################################################
 
-run_name = 'run5'
-data_path = os.getcwd()+'/data'
-traj = md.load_trr(data_path+'/nvt-pr_'+run_name+'.trr', top = data_path+'/water-sol_'+run_name+'.gro')
+run_name = 'run4'
+data_path = '/home/shenglan/MD_simulations/water_box/cubic_1nm_run4'
+traj = md.load_trr(data_path+'/nvt-pr_run4.trr', top = data_path+'/water-sol_run4.gro')
 print ('here is some info about the trajectory we are looking at:')
 print traj
 ws = WaterStats(traj,run_name)
@@ -42,11 +42,15 @@ cut_off = 0.5
 
 tic = time.clock()
 for this_frame in range(ws.n_frames):
+    tic_loop = time.clock()
+    print 'finding tthds for frame %d' % this_frame
     if str(this_frame) in ws.nearest_tthds:
         pass
     else:
         tthds = ws.make_nearest_nb_tthds(cut_off,this_frame)
         ws.nearest_tthds.create_dataset(str(this_frame),data = tthds)
+    toc_loop = time.clock()
+    print('Total time for this frame: %.2f' %(toc_loop-tic_loop)) 
 
 toc = time.clock()
 print('Total time: %.2f' %(toc-tic))
