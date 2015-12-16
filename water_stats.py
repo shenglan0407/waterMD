d84f6c8d8de27c6523561a4fa63ba281815af6be
shenglan <shenglan@stanford.edu>
2015-12-16 11:48:07 -0800

    added more documentation details

--- water_stats.py
+++ water_stats.py
@@ -301,6 +301,19 @@ class WaterStats:
         return tthds 
     
     def make_pairs(self,vertex_ind,cut_off,frame_ind):
+        """Returns a list of vectors that connect pairs of water molecules that are 
+        within a cutoff distances of one water molecules
+        
+        Parameters
+        ----------
+        vertex_ind : int
+            index of the water molecule whose neighbors one want to find and form pairs with
+        cut_off : float
+            distance in nm within which to search for neighbors
+        frame_ind : int
+            index of simulation in which to make pairs
+        
+        """
         nbs = md.compute_neighbors(self.traj[frame_ind],cut_off,[vertex_ind],haystack_indices = self.water_inds)[0]
 
         pairs = []
@@ -340,18 +353,26 @@ class WaterStats:
             frame_steps = frame_steps[:-1]
         
         return np.cumsum(frame_steps)
-        
-    def assign_all_tthds(self):
-        pass
+
         
     def compute_term_four_point(self,q_pair,tthd_pair):
         return (1+np.cos(np.dot(q_pair[0],tthd_pair[0])))*(1+np.cos(np.dot(q_pair[1],tthd_pair[1])))
     
     def four_point_struct_factor(self,qs,cut_off,frame_ind,nearest_nb):
-        """
-        Sums all the fourier terms in one frame of simulation
-        qs: np.array, pairts of (q1,q2)
-        frame_ind: ind of a single frame
+        """Computes the average correlator of tthds from a single simulation frame
+        
+        Parameters
+        ----------
+        qs : np.array, (n,2,3)
+            pairs of (q1,q2) for computing correlator. n is the number of different pairs
+            to compute for.
+        cut_off : float
+            cuttoff distance for looking for nearest neighbors. Should be set such that at
+            least 3 waters can be found within that distance
+        frame_ind: int
+            index of single frame to compute for
+        nearest_nb : bool
+            if True, only use the nearest neighbors to form tthds
         """
         if nearest_nb:
             if str(frame_ind) in self.nearest_tthds:
@@ -415,28 +436,39 @@ class WaterStats:
         pass
 
     def correlator(self,q,wavelength,frames,phi,cut_off = 0.5,nearest_nb=True,output = None):
-        """
+        """Computes 4-point correlator and saves results from each simulation frames in .csv file
         Assume incident beam is along the z axis and water box sample is at origin
         
-        q: magnitude of the q vectors, assumed to be same for now
-        theta_1: 2*theta_1 is the angle between the incident beam and the q vector 
-        dt: average time between frames of simulation to sample
-        cut_off: distance cutoff for making tthds. default is 0.5 nm
-        
+        Parameters
+        ----------
+        q : float
+            magnitude of the q vectors, assumed to be same for now (auto-correlator) 
+        wavelength : float
+            wavelength in nm of the incident beam
+        frames : list
+            indices of frames in which to sample tthds and compute correlator
+        phi : list
+            angles in radian between q1 and q2 in the xy-plane (detector plane). It's used
+            to generate q2 from q1, which is fixed.
+        cut_off: float
+            distance cutoff for making tthds. default 0.5 nm
+        nearest_nb : bool
+            If True only use the nearest three nbs to form tthds, default True
+        output : str
+            File name to save the results in, default None.
         """
-       
-        
-        
-        
-
+ 
         print "frames used for averaging..."
         print frames
+        
         q_beam=2.0*np.pi/wavelength
-    
+        
+        # generate q1 and many q2s
         q1 = np.array([q*np.sqrt(1-(q/(2.*q_beam))**2.0),0,-q**2.0/(2.0*q_beam)])
         q2 = np.array([np.array([q*np.sqrt(1-(q/(2.*q_beam))**2.0)*np.cos(this_phi),q*np.sqrt(1-(q/(2.*q_beam))**2.0)*np.sin(this_phi),q1[2]]) for this_phi in phi])
-    
+        # make pairs of q1 and q2
         qs = np.array([[q1,this_q2] for this_q2 in q2])
+        
         if output == None:
             output_path = os.getcwd()+'/computed_results/corr_'+self.run_name+'.csv'
         else:
@@ -500,17 +532,35 @@ class WaterStats:
     
     
     def find_nearest_nbs(self,cut_off,frame_ind,N_nbs):
-        '''Finds the N nearest neighbors of all waters in a single frame
-        '''
+        """Finds the N nearest neighbors of all waters in a single simulation frame and returns
+        all unique nearest-neighbor tthds as an (n,4) array. n is the number of tthds found in 
+        this frame. Every tthd is represented by 4 indices of the water molecules (oxygens) that
+        are the for vertices. More generally, this function be used to form ensembles with 
+        (N_nbs+1) vertices. The numpy array returned will have shape (n, N_nbs+1).
+        
+        Parameters
+        ----------
+        cut_off : float
+            cuttoff distance for looking for nearest neighbors. Should be set such that at
+            least N_nbs waters can be found within that distance
+        frame_ind : int
+            the index of the frame in which to find nearest neighbors
+        N_nbs : int
+            the number of nearest neighbors to return. set to 3 in this case since we are 
+            interest in 4-point correlators.
+        """
         nearest_nbs = []
         frame = self.traj[frame_ind]
         
         for this_ind in self.water_inds:
+            # find all the neighbors with the cut_off distance for each water molecule
             nbs = md.compute_neighbors(frame,
         cut_off,[this_ind],haystack_indices = self.water_inds)[0]
             
             while len(nbs)!= N_nbs:
                 if len(nbs) > N_nbs:
+                    # compute all the distance between neighbors and only keep the closest
+                    # N_nbs ones
                     pairs = [[this_ind,this_nb] for this_nb in nbs]
                     distances=md.compute_distances(frame,pairs)[0]
                     min_three = np.argsort(distances)[:][:N_nbs]
@@ -518,17 +568,36 @@ class WaterStats:
                 
                 else:
                     print 'increase cut_off!'
+            # keep nbs a list so append can happen
             nbs = [nbs[ii] for ii in range(len(nbs))]
+            # add the ind of the water for whom neighbors have been found to the list
             nbs.append(this_ind)
+            # sort the list so it can be comapred to set of neighbors we already found
             nbs.sort()
+            
             if nbs in nearest_nbs:
+                # if this set of nearest neighbors already exist, don't added it to the list
                 print "not unique"
             else:
+                # only add to list if unique
                 nearest_nbs.append(nbs)
-                
+        # return list as an numpy array
         return np.array(nearest_nbs)
     
     def make_nearest_nb_tthds(self,cut_off,frame_ind):
+        """Returns a list of nearest_nb tthds from a single simulation frame. Each tthd  is 
+        represented by two vectors, r_ij and r_kl. r_ij = (r_i - r_j) and r_kl = (r_k-r_l) 
+        while r_i is the position vector of indexed-i water molecule etc. Also write the four
+        position vecotrs of the four vertices to a pdb format file.
+        
+        Parameters
+        ----------
+        cut_off : float
+            cuttoff distance for looking for nearest neighbors. Should be set such that at
+            least 3 waters can be found within that distance
+        frame_ind : int
+            the index of the frame in which to find nearest neighbor tthds
+        """
         nbs = self.find_nearest_nbs(cut_off,frame_ind,3)
         
         tthds = []
@@ -541,9 +610,12 @@ class WaterStats:
             r2 = xyz_pos[0,this_nb[1],:]
             r3 = xyz_pos[0,this_nb[2],:]
             r4 = xyz_pos[0,this_nb[3],:]
-        
+            
+            # check if any of the nearest neighbors are on the other side of the simulation box
             dr = r1-r2
             for ii in range(3):
+                # if x, y or z coordinate is greater than half the simulation box size, correct
+                # for periodic boundary condition by 'folding' the position vector over
                 if np.abs(dr[ii]) > half_box:
                     if r2[ii]<r1[ii]:
                         r2[ii] = r2[ii]+half_box*2.
@@ -573,6 +645,8 @@ class WaterStats:
             if self.pdb_tthds == None:
                 pass
             else:
+                # store the coordinates the four vertices in pdb format. The center of tthd
+                # always set to origin
                 self.tthd_counter += 1
                 centeroid = (r1+r2+r3+r4)/4.
                 r1=r1-centeroid
