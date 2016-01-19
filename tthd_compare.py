4ca2ee76c296bd6b1eaeafabc5a9a4380ad95016
shenglan <shenglan@stanford.edu>
2016-01-19 15:17:20 -0800

    comparing <c12> results by using different sets of tthds from run7

--- tthd_compare.py
+++ tthd_compare.py
@@ -11,10 +11,10 @@ print ('here is some info about the trajectory we are looking at:')
 print traj
 test = WaterStats(traj,'run7',read_mod = 'r')
 
-q=1/0.23*np.pi*2.0
+q=1/0.465*np.pi*2.0
 phis = np.linspace(0,np.pi,30)
 
 tic = time.clock()
-test.correlator(q,0.1,['tthdSet1','tthdSet2','tthdSet3'],phis,output='run7_corr_test.csv',test_dataset=True)
+test.correlator(q,0.1,['tthdSet1','tthdSet2','tthdSet3'],phis,output='run7_corr_test_q0.465.csv',test_dataset=True)
 toc=time.clock()
 print 'time taken %g second' % (toc-tic)
